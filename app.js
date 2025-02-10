171040414ed518bef10f9350281d341b5febb78e
Flaviasmatt <flaviasmattisson@gmail.com>
2025-02-10 13:31:26 +0100

    Initial commit - Setup do projeto

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+const sequelize = require('./config/database');
+const User = require('./models/User'); // 🔹 Importamos o modelo User
+
+// Sincronizar o banco de dados e criar tabelas
+sequelize.sync()
+    .then(() => {
+        console.log("✅ Banco de dados sincronizado e tabelas criadas!");
+    })
+    .catch((error) => {
+        console.error("❌ Erro ao sincronizar banco de dados:", error);
+    });
+
+module.exports = app;
